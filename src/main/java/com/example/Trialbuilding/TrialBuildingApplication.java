0b523af5f8b06498acadd8ada7b699ff1cbe2043
Dmitry N <70796439+Rudolfff@users.noreply.github.com>
2023-04-08 11:53:47 +0300

    Initial commit

---
+++ src/main/java/com/example/Trialbuilding/TrialBuildingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Trialbuilding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrialBuildingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrialBuildingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Trialbuilding/TrialBuildingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Trialbuilding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialBuildingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
